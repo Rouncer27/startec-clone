8bf9e67303dcd223a49a832cbde27e3bfa755dde
Trevor Rounce <trevor@switchbackcreative.ca>
2024-10-18 10:15:14 -0600

    working on the home page components

---
+++ src/components/HomePage/HeroSlider/HeroSlider.jsx
@@ -0,0 +1,12 @@
+import "./heroSlider.scss";
+
+const HeroSlider = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Hero Slider</h1>
+    </div>
+  );
+};
+
+export default HeroSlider;
---
+++ src/components/HomePage/MarketsSlider/MarketsSlider.jsx
@@ -0,0 +1,12 @@
+import "./marketsSlider.scss";
+
+const MarketsSlider = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Markets Slider</h1>
+    </div>
+  );
+};
+
+export default MarketsSlider;
