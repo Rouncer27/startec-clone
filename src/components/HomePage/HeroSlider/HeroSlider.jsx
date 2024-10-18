e48fec00f7a5525af885f1e746c4cf702667d2cd
Trevor Rounce <trevor@switchbackcreative.ca>
2024-10-18 14:13:27 -0600

    components setup

--- src/components/HomePage/HeroSlider/HeroSlider.jsx
+++ src/components/HomePage/HeroSlider/HeroSlider.jsx
@@ -1,10 +1,31 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import "./heroSlider.scss";
+
+const settings = {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  draggable: true,
+  infinite: true,
+  speed: 500,
+  autoplay: false,
+  autoplaySpeed: 10000,
+  centerMode: false,
+  arrows: false,
+  dots: true,
+  pauseOnHover: true,
+};
 
 const HeroSlider = (props) => {
   console.log(props);
   return (
     <div>
-      <h1>Hero Slider</h1>
+      <div>
+        <h1>Hero Slider</h1>
+      </div>
     </div>
   );
 };
