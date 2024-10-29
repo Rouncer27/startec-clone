84560a3ff80a4e488853a8a9edce9c226ed56c80
Trevor Rounce <trevor@switchbackcreative.ca>
2024-10-29 11:07:00 -0600

    added content to the markets posts

--- src/data/marketsPost/queries.js
+++ src/data/marketsPost/queries.js
@@ -11,3 +11,35 @@ export const Hero_Image_Query = `
     }
     
 `;
+
+export const Intro_Title_Query = `
+    introTitle: introTitleComponent {
+        title
+    }
+`;
+
+export const Top_Content_Query = `
+    topContent: topContentComponent {
+        activateThisComponent
+        content
+    }
+`;
+
+export const Image_Divider_Query = `
+    imageDivider: imageDividerComponent {
+        activateThisComponent
+        image {
+            node {
+                altText
+                sourceUrl
+            }
+        }
+    }
+`;
+
+export const Bottom_Content_Query = `
+    bottomContent: bottomContentComponent {
+        activateThisComponent
+        content
+    }
+`;
