721f26bf4459bdba7ef1c5045b06ed0ceba936ad
Trevor Rounce <trevor@switchbackcreative.ca>
2024-11-01 16:07:06 -0600

    contact form functions

--- netlify/functions/contact.mjs
+++ netlify/functions/contact.mjs
@@ -10,8 +10,9 @@ exports.handler = async function (req, context) {
   formDataArray.forEach((field) => {
     bodyFormData.append(field[0], field[1]);
   });
-  console.log("Sending the form!");
-  const response = await axios.post(FORM_POST_URL, formData, config);
+  bodyFormData.append("_wpcf7_unit_tag", "9a05843");
+
+  const response = await axios.post(FORM_POST_URL, bodyFormData, config);
   console.log("response", response.status, response.data);
 
   return {
