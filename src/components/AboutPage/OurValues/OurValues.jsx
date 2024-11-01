558e3191b87ea4392a5bd16c898959739e845456
Trevor Rounce <trevor@switchbackcreative.ca>
2024-11-01 10:15:30 -0600

    our values component

--- src/components/AboutPage/OurValues/OurValues.jsx
+++ src/components/AboutPage/OurValues/OurValues.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import "./OurValues.scss";
 
 const OurValues = ({ data }) => {
-  console.log("OurValues: ", data);
+  const [activeValue, setActiveValue] = useState(0);
   return (
-    <div>
-      <h1>Our Values</h1>
+    <div className="our-values">
+      <div className="our-values-wrapper">
+        <div className="our-values-title">
+          <h2>Our Values</h2>
+        </div>
+        <div className="our-values-main">
+          <div className="our-values-titles">
+            {data.values.map((value, index) => (
+              <button
+                className={`${activeValue === index ? "active-value-button" : ""}`}
+                type="button"
+                key={index}
+                disabled={activeValue === index}
+                onClick={() => setActiveValue(index)}
+              >
+                {value.title}
+              </button>
+            ))}
+          </div>
+          <div className="our-values-content">
+            {data.values.map((value, index) => (
+              <div
+                key={index}
+                className={`our-values-content-container${activeValue === index ? " active-value" : ""}`}
+                dangerouslySetInnerHTML={{ __html: value.content }}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
