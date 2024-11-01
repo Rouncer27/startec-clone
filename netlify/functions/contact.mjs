c7e4356bcc0d65722e19a06f12afd1e663a1c5a3
Trevor Rounce <trevor@switchbackcreative.ca>
2024-11-01 15:18:59 -0600

    contact form functions

---
+++ netlify/functions/contact.mjs
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+exports.handler = async function (req, context) {
+  const FORM_POST_URL = `https://startec.swbdatabases2.ca/wp-json/contact-form-7/v1/contact-forms/543/feedback`;
+  const config = { headers: { "Content-Type": "multipart/form-data" } };
+  const formData = JSON.parse(req.body);
+
+  const formDataArray = Object.entries(formData);
+  const bodyFormData = new FormData();
+  formDataArray.forEach((field) => {
+    bodyFormData.append(field[0], field[1]);
+  });
+
+  const response = await axios.post(FORM_POST_URL, formData, config);
+  console.log("response", response.status, response.data);
+
+  return {
+    statusCode: response.status,
+    body: JSON.stringify({
+      data: response.data,
+    }),
+  };
+};
