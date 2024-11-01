51de1864767fe2f114195129c6e7deeb7f128f6c
Trevor Rounce <trevor@switchbackcreative.ca>
2024-11-01 09:51:41 -0600

    setup all new components for the about page

---
+++ src/components/AboutPage/OurValues/OurValues.jsx
@@ -0,0 +1,12 @@
+import "./OurValues.scss";
+
+const OurValues = ({ data }) => {
+  console.log("OurValues: ", data);
+  return (
+    <div>
+      <h1>Our Values</h1>
+    </div>
+  );
+};
+
+export default OurValues;
--- src/data/about/queries.js
+++ src/data/about/queries.js
@@ -18,3 +18,93 @@ export const Intro_Query = `
         displayLetsConnectIcon
     }
 `;
+
+export const Our_Values_Query = `
+    ourValues: ourValuesComponent {
+        values {
+            title
+            content
+        }
+    }
+`;
+
+export const Executive_Team_Query = `
+    executiveTeam: executiveTeamComponent {
+        members {
+            member {
+                edges {
+                    node {
+                        ... on Bio {
+                        title
+                        slug
+                        biosPost {
+                            featuredImageComponent {
+                                image {
+                                    node {
+                                    altText
+                                    sourceUrl
+                                    }
+                                }
+                                titleJob
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export const Leadership_Team_Query = `
+    leadershipTeam: leadershipTeamComponent {
+        members {
+            member {
+                edges {
+                    node {
+                        ... on Bio {
+                        title
+                        slug
+                        biosPost {
+                            featuredImageComponent {
+                                image {
+                                    node {
+                                    altText
+                                    sourceUrl
+                                    }
+                                }
+                                titleJob
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export const Team_Video_Query = `
+    teamVideo: teamVideoComponent {
+        teamVideoEmbed
+    }
+`;
+
+export const Small_Call_Action_Query = `
+    smallCallAction: smallCallToAction {
+        title
+        content
+        iconImage {
+          node {
+            altText
+            sourceUrl
+          }
+        }
+        backgroundImage {
+          node {
+            altText
+            sourceUrl
+          }
+        }
+      }
+`;
