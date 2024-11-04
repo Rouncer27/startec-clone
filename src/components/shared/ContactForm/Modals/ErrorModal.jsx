31e67062cb0e8c2dc752e61fdf3bca8792fd0655
Trevor Rounce <trevor@switchbackcreative.ca>
2024-11-04 08:27:35 -0700

    contact form modals

--- src/components/shared/ContactForm/Modals/ErrorModal.jsx
+++ src/components/shared/ContactForm/Modals/ErrorModal.jsx
@@ -1,8 +1,21 @@
+import "./formModals.scss";
+import ErrorIcon from "./ErrorIcon";
+
 const ErrorModal = ({ handleErrorModalClose }) => {
   return (
-    <div>
-      <h1>Error With The Contact Form!</h1>
-      <button onClick={handleErrorModalClose}>Close</button>
+    <div className="form-modal error-modal">
+      <div className="form-modal-inner error-modal-inner">
+        <div className="form-modal-inner-icon error-modal-inner-icon">
+          <ErrorIcon />
+        </div>
+        <div className="form-modal-inner-content error-modal-inner-content">
+          <p>Error With The Contact Form!</p>
+        </div>
+        <div className="form-modal-inner-button error-modal-inner-button">
+          <button onClick={handleErrorModalClose}>Close</button>
+        </div>
+      </div>
+      <div className="form-modal-background error-modal-background" />
     </div>
   );
 };
--- src/components/shared/ContactForm/Modals/SuccessModal.jsx
+++ src/components/shared/ContactForm/Modals/SuccessModal.jsx
@@ -1,8 +1,21 @@
+import "./formModals.scss";
+import SuccessIcon from "./SuccessIcon";
+
 const SuccessModal = ({ handleSuccessModalClose }) => {
   return (
-    <div>
-      <h1>Success you form has been sent!</h1>
-      <button onClick={handleSuccessModalClose}>Close</button>
+    <div className="form-modal success-modal">
+      <div className="form-modal-inner success-modal-inner">
+        <div className="form-modal-inner-icon success-modal-inner-icon">
+          <SuccessIcon />
+        </div>
+        <div className="form-modal-inner-content success-modal-inner-content">
+          <p>Your form has been successfully sent.</p>
+        </div>
+        <div className="form-modal-inner-button success-modal-inner-button">
+          <button onClick={handleSuccessModalClose}>Close</button>
+        </div>
+      </div>
+      <div className="form-modal-background success-modal-background" />
     </div>
   );
 };
