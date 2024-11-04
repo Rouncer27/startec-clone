3448b3687583fb2f5c0475553db1a18129efd249
Trevor Rounce <trevor@switchbackcreative.ca>
2024-11-04 11:57:28 -0700

    error handleing

--- src/components/shared/ContactForm/functions/submitForm.js
+++ src/components/shared/ContactForm/functions/submitForm.js
@@ -39,67 +39,81 @@ const submitForm = async (
         `Form was not submitted because the server took too long to respond or your browser has block the request to the server. - Timeout Error. Please try again.`,
       );
     }, 10000);
+    try {
+      const formDataArray = Object.entries(formData);
+      const bodyFormData = new FormData();
+      formDataArray.forEach((field) => {
+        bodyFormData.append(field[0], field[1]);
+      });
 
-    const formDataArray = Object.entries(formData);
-    const bodyFormData = new FormData();
-    formDataArray.forEach((field) => {
-      bodyFormData.append(field[0], field[1]);
-    });
+      const response = await fetch("/.netlify/functions/contact", {
+        method: "POST",
+        body: JSON.stringify(formData),
+      }).then((response) => {
+        console.log("response before JSON: ", response);
+        if (response.status === 404) {
+          throw new Error(
+            `Contact Form was not sent because the server is not found - ${response.status}. Please try again.`,
+          );
+        } else if (response.status !== 200) {
+          throw new Error(
+            `Contact Form was not sent because something went wrong with the request to the server - ${response.status}. Please try again.`,
+          );
+        }
 
-    const response = await fetch("/.netlify/functions/contact", {
-      method: "POST",
-      body: JSON.stringify(formData),
-    }).then((response) => {
-      console.log("response before JSON: ", response);
-      if (response.status === 404) {
+        return response.json();
+      });
+
+      console.log("response: ", response);
+
+      if (response.data.status === "mail_sent") {
+        clearTimeout(timeoutID);
+        setFormStatus({
+          ...formStatus,
+          submitting: false,
+          errorWarnDisplay: false,
+          success: true,
+          captachError: false,
+          validationFailedError: false,
+          timeOutError: false,
+          unknownError: false,
+          errors: [],
+        });
+        clearFormFields(setFormData);
+      } else if (response.data.status === "validation_failed") {
+        clearTimeout(timeoutID);
+        setFormStatus({
+          ...formStatus,
+          submitting: false,
+          errorWarnDisplay: true,
+          success: false,
+          validationFailedError: true,
+          timeOutError: false,
+          unknownError: false,
+          errors: response.data.invalid_fields,
+        });
+      } else {
+        clearTimeout(timeoutID);
         throw new Error(
-          `Contact Form was not sent because the server is not found - ${response.status}. Please try again.`,
-        );
-      } else if (response.status !== 200) {
-        throw new Error(
-          `Contact Form was not sent because something went wrong with the request to the server - ${response.status}. Please try again.`,
+          `Contact Form was not sent - CMS Contact Form Error:- ${response?.data?.status}`,
         );
       }
-
-      return response.json();
-    });
-
-    console.log("response: ", response);
-
-    if (response.data.status === "mail_sent") {
-      clearTimeout(timeoutID);
-      setFormStatus({
-        ...formStatus,
-        submitting: false,
-        errorWarnDisplay: false,
-        success: true,
-        captachError: false,
-        validationFailedError: false,
-        timeOutError: false,
-        unknownError: false,
-        errors: [],
-      });
-      clearFormFields(setFormData);
-    } else if (response.data.status === "validation_failed") {
+    } catch (error) {
+      console.log("error: ", error);
       clearTimeout(timeoutID);
       setFormStatus({
         ...formStatus,
         submitting: false,
         errorWarnDisplay: true,
         success: false,
-        validationFailedError: true,
+        validationFailedError: false,
         timeOutError: false,
-        unknownError: false,
-        errors: response.data.invalid_fields,
+        unknownError: true,
+        errors: [{ unknownErrorMessage: error }],
       });
-    } else {
-      clearTimeout(timeoutID);
-      throw new Error(
-        `Contact Form was not sent - CMS Contact Form Error:- ${response?.data?.status}`,
-      );
     }
   } catch (error) {
-    console.log("error: ", error);
+    console.log("Timeout error: ", error);
     clearTimeout(timeoutID);
     setFormStatus({
       ...formStatus,
@@ -107,9 +121,9 @@ const submitForm = async (
       errorWarnDisplay: true,
       success: false,
       validationFailedError: false,
-      timeOutError: false,
-      unknownError: true,
-      errors: [{ unknownErrorMessage: error }],
+      timeOutError: true,
+      unknownError: false,
+      errors: [{ errorMessageTimeout: error }],
     });
   }
 };
