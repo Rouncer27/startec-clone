09ae71eac330b4fe7cd1fbed6a48f41d97908126
Trevor Rounce <trevor@switchbackcreative.ca>
2024-11-04 12:20:54 -0700

    error handleing

--- netlify/functions/contact.mjs
+++ netlify/functions/contact.mjs
@@ -12,7 +12,7 @@ exports.handler = async function (req, context) {
   });
 
   // Just a test to see if the timeout is working //
-  await new Promise((resolve) => setTimeout(resolve, 15000));
+  // await new Promise((resolve) => setTimeout(resolve, 15000));
 
   const response = await axios.post(FORM_POST_URL, bodyFormData, config);
 
