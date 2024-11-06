89dc8bc8be30da4c61a3659a6b19eeec8a1964bb
Trevor Rounce <trevor@switchbackcreative.ca>
2024-11-06 09:47:14 -0700

    client provided hero images

--- src/components/MarketsPost/RelatedProjects/RelatedProjects.jsx
+++ src/components/MarketsPost/RelatedProjects/RelatedProjects.jsx
@@ -3,7 +3,6 @@ import Slider from "react-slick";
 import Project from "./Project";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { act } from "react";
 
 const settings = {
   slidesToShow: 3,
