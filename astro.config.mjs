187d731f0c29924fe05c4f658c165509f0132a21
Trevor Rounce <trevor@switchbackcreative.ca>
2024-11-07 10:06:54 -0700

    client provided hero images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://startec.netlify.app/",
-
+  integrations: [react()],
   image: {
     domains: ["startec.swbdatabases2.ca/"],
+    remotePatterns: [{ protocol: "https" }],
   },
-
-  integrations: [react()],
 });
