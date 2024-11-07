1c3c8393b286b1dfb1eb269b30d19199370b1115
Trevor Rounce <trevor@switchbackcreative.ca>
2024-11-07 16:15:06 -0700

    optimizing images for products

--- netlify/functions/contact.mjs
+++ netlify/functions/contact.mjs
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-exports.handler = async function (req, context) {
+const handler = async function (req, context) {
   const FORM_POST_URL = `https://startec.swbdatabases2.ca/wp-json/contact-form-7/v1/contact-forms/546/feedback`;
   const config = { headers: { "Content-Type": "multipart/form-data" } };
   const formData = JSON.parse(req.body);
@@ -23,3 +23,5 @@ exports.handler = async function (req, context) {
     }),
   };
 };
+
+export default handler;
