80bf64844fa04774fc1584f49abf6546e9d1359c
Trevor Rounce <trevor@switchbackcreative.ca>
2024-12-17 14:15:16 -0700

    SEO meta tags for Twitter cards and OG tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://startec.netlify.app/",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   image: {
     domains: ["startec.swbdatabases2.ca/"],
     remotePatterns: [{ protocol: "https" }],
