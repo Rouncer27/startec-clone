12460b318e3f96cde41b257196291c3f327fe2b0
Trevor Rounce <trevor@switchbackcreative.ca>
2026-08-24 10:36:50 -0600

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
-import sitemap from "@astrojs/sitemap";
+// import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://startec.ca/",
-  integrations: [react(), sitemap()],
+  integrations: [react()],
   image: {
     domains: ["startec-clone.swbdatabases3.ca/"],
     remotePatterns: [{ protocol: "http" }],
